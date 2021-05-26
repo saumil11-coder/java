870da2a035ba1b06aeed1a09ad0422c1258c8450
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2021-05-26 20:05:37 +0530

    Add files via upload

---
+++ quicks.java
@@ -0,0 +1,53 @@
+package array1;
+import java.io.*;
+public class quicks {
+static void swap(int arr[],int i,int j)
+{
+	int temp;
+	temp=arr[i];
+	arr[i]=arr[j];
+	arr[j]=temp;
+}
+static int partition(int arr[],int low,int high)
+{
+	int pivot=arr[high];
+	int i=(low-1);
+	for(int j=low;j<=high-1;j++)
+	{
+		if(arr[j]<pivot)
+		{
+			i++;
+			swap(arr,i,j);
+			
+		}
+	}
+	swap(arr,i+1,high);
+	return (i+1);
+}
+static void quicksort(int arr[],int low,int high)
+{
+	if(low<high)
+	{
+		int pi=partition(arr,low,high);
+		quicksort(arr,low,pi-1);
+		quicksort(arr,pi+1,high);
+	}
+}
+static void printarray(int arr[],int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		System.out.print(arr[i]+" ");
+		System.out.println();
+	}
+	
+}
+	public static void main(String[] args) {
+		int[] arr = { 10, 7, 8, 9, 1, 5 };
+		int n=arr.length;
+		quicksort(arr,0,n-1);
+		System.out.println("sorted array");
+		printarray(arr,n);
+	}
+
+}
