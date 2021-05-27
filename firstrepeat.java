23c03b71f87e53c4fddbe4a99b0d4891cd52bd8c
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2021-05-27 11:54:37 +0530

    Add files via upload

--- firstrepeat.java
+++ firstrepeat.java
@@ -3,24 +3,24 @@ import java.util.*;
 public class firstrepeat {
 	static void printFirstRepeating(int arr[])
     {
-        // Initialize index of first repeating element
+        
         int min = -1;
  
-        // Creates an empty hashset
+       
         HashSet<Integer> set = new HashSet<>();
  
-        // Traverse the input array from right to left
+      
         for (int i=arr.length-1; i>=0; i--)
         {
-            // If element is already in hash set, update min
+           
             if (set.contains(arr[i]))
                 min = i;
- 
-            else   // Else add element to hash set
+
+            else   
                 set.add(arr[i]);
         }
  
-        // Print the result
+       
         if (min != -1)
           System.out.println("The first repeating element is " + arr[min]);
         else
